0cced8fec0b84d4365b6f49396e281e97f8c5bc2
huseynliir <Isaq.Huseynli@kapitalbank.az>
2023-03-06 17:30:23 +0400

    GCD_LCM_2

--- GCD_LCM/src/Main.java
+++ GCD_LCM/src/Main.java
@@ -2,25 +2,37 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
+    public static  int gcd(int a,int b){
+        int gcd=0;
+        int c;
+        if (a>b){
+            c=b;
+        }
+        else {
+            c=a;
+        }
+
+        for (int i=1;i<=c/2;i++){
+            if (a%i==0 &&b%i==0){
+                gcd=i;
+            }
+
+        }
+        return gcd;
+
+    }
+    public static  int lcm(int a,int b){
+        int lcm=0;
+        int c;
+        lcm=a*b/gcd(a,b);
+        return lcm;
+
+    }
     public static void main(String[] args) {
-        List<Integer>nums=new ArrayList<>() ;
      int a=147;
      int b=196;
-     int gcd=0;
-     int c;
-     if (a>b){
-         c=b;
-     }
-     else {
-         c=a;
-     }
 
-     for (int i=1;i<=c/2;i++){
-         if (a%i==0 &&b%i==0){
-            gcd=i;
-         }
-
-     }
-        System.out.println(gcd);
+        System.out.println(gcd(a,b));
+        System.out.println(lcm(a,b));
     }
 }
