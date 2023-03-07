d25dff3dd0cb1dcfbaae887818516bb663e1fe5b
huseynliir <Isaq.Huseynli@kapitalbank.az>
2023-03-07 18:22:04 +0400

    obms_1

---
+++ obms/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Person p=new Person(1,"Yuh","akjsss","1233343","wed");
+        p.setEmail("fd");
+        System.out.println(p.getEmail());
+    }
+}
