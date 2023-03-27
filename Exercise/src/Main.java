d57bedeef18ed9da4af43ddc154676c32f4bef54
huseynliir <Isaq.Huseynli@kapitalbank.az>
2023-03-27 11:45:30 +0400

    Exercise

--- Exercise/src/Main.java
+++ Exercise/src/Main.java
@@ -13,7 +13,7 @@ public class Main {
         int arr[]={2,3,4,1,5,6};
         for (int i=0;i<arr.length;i++){
             for (int j=0;j<arr.length;j++){
-                  if(arr[j]<arr[i]){
+                  if(arr[j]>arr[i]){
                       int temp=arr[j];
                       arr[j]=arr[i];
                       arr[i]=temp;
