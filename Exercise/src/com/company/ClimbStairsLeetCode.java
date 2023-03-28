5dcc6315cafdaaed945c7beaa34455c51a59a5ca
isaq1997 <125879696+isaq1997@users.noreply.github.com>
2023-03-28 23:14:34 +0400

    Update ClimbStairsLeetCode.java

--- Exercise/src/com/company/ClimbStairsLeetCode.java
+++ Exercise/src/com/company/ClimbStairsLeetCode.java
@@ -1,8 +1,27 @@
 package com.company;
 
 public class ClimbStairsLeetCode {
-    public static void main(String[] args) {
+    public static int climbStairs(int n) {
+        int a1 = 1;
+        int a2 = 2;
+        int res = 0;
+        //int n=3;
 
+        if (n == 1) {
+            return 1;
+        } else if (n == 2) {
+            return 2;
+        }
+
+        for (int i = 1; i <= n - 2; i++) {
+            res = a1 + a2;
+            a1 = a2;
+            a2 = res;
+        }
+        return res;
+    }
+        public static void main(String[] args) {
+            System.out.println(climbStairs(6));
 
     }
 }
